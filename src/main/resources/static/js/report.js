e2028f00b9d731cdd8a0bfde012d34bbcd1b12b2
zhu-gang <632452599@qq.com>
2019-07-02 16:37:45 +0800

    2019.7.2 zhugang1

--- src/main/resources/static/js/report.js
+++ src/main/resources/static/js/report.js
@@ -364,7 +364,7 @@ layui.use(['jquery','form','upload','layer','layedit'], function(){
 		 //dfid为流程定义id（暂时就是dfid="processPure2:4:47506"）
 		 $.ajax({
 		     type: "POST"
-		     ,url: '/iot_process/process/processPure2:13:170004'    //dfid为流程定义id（暂时就是dfid="processPure2:4:47506"）
+		     ,url: '/iot_process/process/processPure2:2:15004'    //dfid为流程定义id（暂时就是dfid="processPure2:4:47506"）
 		     ,data: data.field  //问题上报表单的内容
 		     ,contentType: "application/x-www-form-urlencoded"
 		     ,dataType: "json"
