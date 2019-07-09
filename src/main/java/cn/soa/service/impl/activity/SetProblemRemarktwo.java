c4cda1f200cf3e168dc39a11456637a5c100e213
BruLo2017 <BurLo_2017@163.com>
2019-07-09 20:06:03 +0800

    后置任务update

--- src/main/java/cn/soa/service/impl/activity/SetProblemRemarktwo.java
+++ src/main/java/cn/soa/service/impl/activity/SetProblemRemarktwo.java
@@ -32,43 +32,44 @@ public class SetProblemRemarktwo implements ExecutionListener {
 	private static Logger logger = LoggerFactory.getLogger( SetProblemRemarktwo.class );
 
 	/**
-	 * 将责任人写入数据库
+	 * 
 	 */
 
 	@Override
 	public void notify(DelegateExecution execution) {
-
 		
-		logger.info( "---------后置任务获取任务---------" );	
+		logger.info( "---------后置任务下一个节点流程---------" );	
 		ProblemInfoSI problemInfoS= SpringUtils.getObject(ProblemInfoSI.class);
 		
-		AcitivityTaskS acitivityTaskS = SpringUtils.getObject(AcitivityTaskS.class);
-		
-		ActivityS activityS = SpringUtils.getObject(ActivityS.class);
+		String name = execution.getCurrentActivityName();
+		/*
+		 * AcitivityTaskS acitivityTaskS = SpringUtils.getObject(AcitivityTaskS.class);
+		 * 
+		 * ActivityS activityS = SpringUtils.getObject(ActivityS.class);
+		 */
 		
 		String piid = execution.getProcessInstanceId();
 		logger.info( "---------获取流程piid："+piid );	
 		
-		String tsid = activityS.getActiveTsidByPiid(piid);
-		logger.info( "---------获取流程tsid："+tsid );	
-		
-		//获取任务名
-		String name = acitivityTaskS.getTaskByTsid(tsid).getName();
-		logger.info( "---------获取流程任务名："+name );	
+		/*
+		 * String tsid = activityS.getTsidByPiid(piid); logger.info(
+		 * "---------获取流程tsid："+tsid );
+		 * 
+		 * //获取任务名 String name = acitivityTaskS.getTaskByTsid(tsid).getName();
+		 */
+		 logger.info( "---------获取流程任务名："+name );
 		
 		if( piid == null ) {
 			logger.info( "---------获取流程piid为空或null------------" );			
 		}else {
 			
-			logger.info( "---------下一个节点名称后置任务更新行数------------2");
 			
-			/*
-			 * ProblemInfo problemInfo = new ProblemInfo(); problemInfo.setPiid(piid);
-			 * problemInfo.setRemarktwo(name); Integer row =
-			 * problemInfoS.changeProblemDescribeByPiid(problemInfo);
-			 * 
-			 * logger.info( "---------下一个节点名称后置任务更新行数------------" + row );
-			 */
+			ProblemInfo problemInfo = new ProblemInfo(); problemInfo.setPiid(piid);
+			problemInfo.setRemarktwo(name); 
+			Integer row = problemInfoS.changeProblemDescribeByPiid(problemInfo);
+			
+			logger.info( "---------下一个节点名称后置任务更新行数------------" + row );
+			
 			
 		}	
 	
